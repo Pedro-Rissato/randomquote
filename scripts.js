f32c96e4da5548563830446765ef1c02f0746ae5
Pedro-Rissato <peh.rissato@gmail.com>
2025-04-24 13:40:18 -0300

    outra api

--- scripts.js
+++ scripts.js
@@ -1,16 +1,25 @@
 function fetchRandomQuote() {
-    fetch("https://api.quotable.io/random")
-    .then (response => Response.json())
-    .then (data => {
-        const quote = data.content;
-        document.getElementById ("quote").textContent = quote;
-
+    fetch("https://api.api-ninjas.com/v1/quotes", {
+      method: "GET",
+      headers: {
+        'X-Api-Key': '/utuzvnY3/Aw7w5ck4DKWA==udmGTR8SbtINIZbZ' // <- coloque sua chave aqui
+      }
     })
-    .catch(error => {
-        console.error("Error fetching quote", error);
-    });
-}
-
-document.getElementById("generator").addEventListener("click",fetchRandomQuote);
-
-fetchRandomQuote()
+      .then(response => response.json())
+      .then(data => {
+        const quote = data[0].quote;
+        const author = data[0].author;
+  
+        document.getElementById("quote").textContent = `"${quote}"`;
+        document.getElementById("author").textContent = `– ${author}`;
+      })
+      .catch(error => {
+        console.error("Erro ao buscar frase", error);
+      });
+  }
+  
+  document.getElementById("generator").addEventListener("click", fetchRandomQuote);
+  
+  // opcional: já exibe uma ao carregar
+  fetchRandomQuote();
+  
